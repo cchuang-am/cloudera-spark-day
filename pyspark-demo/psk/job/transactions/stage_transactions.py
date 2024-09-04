1160f131c6ccbace0f0e61702ef646f34e866e4d
CC Huang <cc.huang@athemaster.com>
2024-09-04 15:06:51 +0800

    use full data

--- pyspark-demo/psk/job/transactions/stage_transactions.py
+++ pyspark-demo/psk/job/transactions/stage_transactions.py
@@ -3,6 +3,6 @@ from psk.config import core_config
 
 
 def load_transactions(spark):
-    files = f"{core_config.DATA_SOURCE()}/transactions_mini"
+    files = f"{core_config.DATA_SOURCE()}/transactions"
 
     return spark.read.parquet(files)
